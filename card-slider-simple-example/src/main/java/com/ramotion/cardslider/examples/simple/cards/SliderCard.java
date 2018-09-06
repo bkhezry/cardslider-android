3a03f933a46c28bee1fad137d4a64b5aa57c7b99
‌Behrouz <bkhezry@gmail.com>
2018-09-06 13:37:29 +0430

    Rearrage code.

--- card-slider-simple-example/src/main/java/com/ramotion/cardslider/examples/simple/cards/SliderCard.java
+++ card-slider-simple-example/src/main/java/com/ramotion/cardslider/examples/simple/cards/SliderCard.java
@@ -1,8 +1,6 @@
 package com.ramotion.cardslider.examples.simple.cards;
 
 import android.graphics.Bitmap;
-import androidx.annotation.DrawableRes;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
@@ -10,51 +8,54 @@ import android.widget.ImageView;
 import com.ramotion.cardslider.examples.simple.R;
 import com.ramotion.cardslider.examples.simple.utils.DecodeBitmapTask;
 
+import androidx.annotation.DrawableRes;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class SliderCard extends RecyclerView.ViewHolder implements DecodeBitmapTask.Listener {
 
-    private static int viewWidth = 0;
-    private static int viewHeight = 0;
+  private static int viewWidth = 0;
+  private static int viewHeight = 0;
 
-    private final ImageView imageView;
+  private final ImageView imageView;
 
-    private DecodeBitmapTask task;
+  private DecodeBitmapTask task;
 
-    public SliderCard(View itemView) {
-        super(itemView);
-        imageView = (ImageView) itemView.findViewById(R.id.image);
-    }
+  public SliderCard(View itemView) {
+    super(itemView);
+    imageView = (ImageView) itemView.findViewById(R.id.image);
+  }
 
-    void setContent(@DrawableRes final int resId) {
-        if (viewWidth == 0) {
-            itemView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                @Override
-                public void onGlobalLayout() {
-                    itemView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+  void setContent(@DrawableRes final int resId) {
+    if (viewWidth == 0) {
+      itemView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        @Override
+        public void onGlobalLayout() {
+          itemView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 
-                    viewWidth = itemView.getWidth();
-                    viewHeight = itemView.getHeight();
-                    loadBitmap(resId);
-                }
-            });
-        } else {
-            loadBitmap(resId);
+          viewWidth = itemView.getWidth();
+          viewHeight = itemView.getHeight();
+          loadBitmap(resId);
         }
+      });
+    } else {
+      loadBitmap(resId);
     }
+  }
 
-    void clearContent() {
-        if (task != null) {
-            task.cancel(true);
-        }
+  void clearContent() {
+    if (task != null) {
+      task.cancel(true);
     }
+  }
 
-    private void loadBitmap(@DrawableRes int resId) {
-        task = new DecodeBitmapTask(itemView.getResources(), resId, viewWidth, viewHeight, this);
-        task.execute();
-    }
+  private void loadBitmap(@DrawableRes int resId) {
+    task = new DecodeBitmapTask(itemView.getResources(), resId, viewWidth, viewHeight, this);
+    task.execute();
+  }
 
-    @Override
-    public void onPostExecuted(Bitmap bitmap) {
-        imageView.setImageBitmap(bitmap);
-    }
+  @Override
+  public void onPostExecuted(Bitmap bitmap) {
+    imageView.setImageBitmap(bitmap);
+  }
 
 }
